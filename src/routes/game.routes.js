456f969bf05c9dc91a2871565b4668969182dab3
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2026-03-25 20:32:56 +0300

    Initial commit

--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -4,11 +4,26 @@ const auth = require("../middleware/auth.middleware");
 
 router.use(auth);
 
-router.get("/", ctrl.getAll);
-router.get("/:id", ctrl.getOne);
-router.post("/", ctrl.create);
-router.put("/:id", ctrl.update);
-router.patch("/:id", ctrl.patch);
-router.delete("/:id", ctrl.remove);
+// helper
+const methodNotAllowed = (methods) => (req, res) => {
+  res.set("Allow", methods.join(", "));
+  return res.status(405).json({ error: "Method Not Allowed" });
+};
+
+// /games
+router
+  .route("/")
+  .get(ctrl.getAll)
+  .post(ctrl.create)
+  .all(methodNotAllowed(["GET", "POST"]));
+
+// /games/:id
+router
+  .route("/:id")
+  .get(ctrl.getOne)
+  .put(ctrl.update)
+  .patch(ctrl.patch)
+  .delete(ctrl.remove)
+  .all(methodNotAllowed(["GET", "PUT", "PATCH", "DELETE"]));
 
 module.exports = router;
